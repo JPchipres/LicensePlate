fae48524084142e411be5c48fbbe767aee99c124
Gilberto Valenzuela <54441538+GilbertoMV@users.noreply.github.com>
2023-05-08 09:59:06 -0600

    logica terminada

--- webapp/assets/js/alerts.js
+++ webapp/assets/js/alerts.js
@@ -1,33 +1,124 @@
-import {setPlateResident, setNewException, getData} from './firebase.js' 
+import {setPlateResident, setNewException, getData, newResident,setPlateVisit} from './firebase.js' 
 
-const table = document.getElementById('datos');
-
-window.addEventListener('DOMContentLoaded', async () =>{
-
-  const querySnapshot = await getData()
-  let i = 1
-  
-  let html = ''
-
-  querySnapshot.forEach(data => {
-    const doc = data.data()
-    const num = i++
-    const timestamp = doc.admission
-    const date = timestamp.toDate()
-    const fechaEntrada = date.toLocaleString()
-    html += `
-              <tr> 
-                  <td>${num}</td>
-                  <td>${doc.placa}</td>
-                  <td>${fechaEntrada}</td>
-                  <td>${doc.estado}</td>
-                  <td>${doc.clase}</td>
-              </tr>
-          `
+$('#nuevoResidente').click(function() {
+  Swal.mixin({
+    background:'#2c2c2c',
+    cancelButtonText:'Cancelar',
+    confirmButtonText: 'Siguiente',
+    confirmButtonColor:'#198754',
+    showCancelButton: true,
+    cancelButtonColor:'#dc3545',
+    allowOutsideClick:false,
+    allowEscapeKey:false,
+  }).queue([
+    {
+      input: 'text',
+      title: 'Nombre Residente',
+      text: 'Ingrese el nombre completo del nuevo residente.',
+      inputAttributes:{
+        minlength: 10,
+        maxlength: 100,
+      },
+      customClass:{
+        input:'capitalize'
+      },
+      inputValidator: (value) => {
+        if (!value) {
+          return 'Este campo es requerido';
+        }else if(value.length <10){
+          return 'Este campo está incompleto';
+        }
+      },
+      // footer: "El formato a seguir es: AAA-00-00"
+    }, //NOMBRE RESIDENTE
+    {
+      input:'text',
+      title: 'Casa Residente',
+      inputAttributes: {
+        maxlength:5,
+        minlength:2
+      },
+      text: 'Escriba el numero de casa del residente a registrar.',
+      inputValidator: (value) => {
+        if (!value) {
+          return 'Este campo es requerido';
+        }else if(value.length <2){
+          return 'Este campo está incompleto';
+        }
+      },
+      customClass:{
+        input:'uppercase'
+      },
+    }, //CASA RESIDENTE
+    {
+      input:'email',
+      title: 'Email Residente',
+      text: 'Escriba el email del residente a registrar',
+      // inputValidator: (value) => {
+      //   if (!value) {
+      //     return 'Este campo es requerido';
+      //   }else if(value.length <10){
+      //     return 'Este campo está incompleto';
+      //   }
+      // }
+    }, //EMAIL RESIDENTE
+    {
+      input:'number',
+      title: 'Teléfono Residente',
+      inputAttributes: {
+        maxlength:10,
+        max:9999999999
+      },
+      text: 'Escriba el numero de teléfono del residente a registrar.',
+      inputValidator: (value) => {
+        if (!value) {
+          return 'Este campo es requerido';
+        }else if(value.length <10){
+          return 'Este campo está incompleto';
+        }else if(value.length >10){
+          return 'Maximo dígitos superado'
+        }
+      }
+    } //TELEFONO RESIDENTE
+  ]).then(data => {
     
-  });
-  table.innerHTML = html;
-})
+    if(data.value) {
+      const nombre = data.value[0].toUpperCase()
+      const casa = data.value[1].toUpperCase()
+      const email = data.value[2].toUpperCase()
+      const telefono = data.value[3]
+      // console.log(nombre,casa,email,telefono)
+      Swal.fire({
+        icon:'info',
+        title: 'Confirmación',
+        html: `
+            ¿Desea dar de alta al cliente: <small>${nombre}</small>, número de casa: <small>${casa}</small>,
+            email: <small>${email}</small> y  teléfono: <small>${telefono}</small>?
+        `,
+        background:'#2c2c2c',
+        confirmButtonColor:'#198754',
+        confirmButtonText: 'Dar de alta',
+        cancelButtonText:'Cancelar',
+        showCancelButton:true,
+        cancelButtonColor:'#dc3545',
+        allowOutsideClick:false,
+        allowEscapeKey:false,
+        showLoaderOnConfirm: true,
+        preConfirm: () => {
+          return new Promise((resolve) => {
+            setTimeout(() => {
+              resolve();
+            }, 2000); // Simulación de una operación asincrónica
+          });
+        }
+      }).then((data) => {
+        if(data.isConfirmed){
+          newResident(nombre,casa,email,telefono)
+        }
+      })
+    }
+  })
+});
 
 $('#nuevaPlacaResidente').click(function() {
   Swal.mixin({
@@ -37,7 +128,6 @@ $('#nuevaPlacaResidente').click(function() {
     confirmButtonColor:'#198754',
     showCancelButton: true,
     cancelButtonColor:'#dc3545',
-    // progressSteps: ['1', '2'],
     allowOutsideClick:false,
     allowEscapeKey:false
   }).queue([
@@ -50,6 +140,9 @@ $('#nuevaPlacaResidente').click(function() {
         minlength: 9,
         maxlength: 9,
       },
+      customClass: {
+        input: 'uppercase'
+      },
       inputValidator: (value) => {
         if (!value) {
           return 'Este campo es requerido';
@@ -60,11 +153,11 @@ $('#nuevaPlacaResidente').click(function() {
       footer: "El formato a seguir es: AAA-00-00"
     },
     {
-      input:'text',
+      input:'number',
       title: 'ID del Residente',
       inputAttributes: {
         maxlength:10,
-        minlength:10
+        max:999999999
       },
       text: 'Escriba el ID del residente al que se le asignará la placa',
       inputValidator: (value) => {
@@ -72,6 +165,8 @@ $('#nuevaPlacaResidente').click(function() {
           return 'Este campo es requerido';
         }else if(value.length <9){
           return 'Este campo está incompleto';
+        }else if(value.length >10){
+          return 'Maximo dígitos superado'
         }
       },
       footer:'El ID residente debe ser el numero de telefono registrado',
@@ -79,13 +174,13 @@ $('#nuevaPlacaResidente').click(function() {
   ]).then(data => {
     
     if(data.value) {
-      const placa = data.value[0]
-      const residentID = (data.value[1])
+      const placa = data.value[0].toUpperCase()
+      const residentID = data.value[1]
       Swal.fire({
         icon:'info',
         title: 'Confirmación',
         html: `
-            ¿Desea dar de alta la placa ${placa} al residente con ID ${residentID}?
+            ¿Desea dar de alta la placa <small>${placa}</small> al residente con ID <small>${residentID}</small>?
         `,
         background:'#2c2c2c',
         confirmButtonColor:'#198754',
@@ -120,18 +215,17 @@ $('#nuevaPlacaVisitante').click(function() {
     confirmButtonColor:'#198754',
     showCancelButton: true,
     cancelButtonColor:'#dc3545',
-    // progressSteps: ['1', '2'],
     allowOutsideClick:false,
     allowEscapeKey:false
   }).queue([
     {
       input: 'text',
       title: 'Placa Visitante',
-      text: 'Escriba los dígitos de la nueva placa del visitante',
-      footer:'Aviso: La placa visitante solo será valida por 1 día.',
+      text: 'Escriba los dígitos de la nueva placa con privilegios de visitante',
+      footer:'Aviso: La placa residente solo permite acceder una vez',
       inputAttributes:{
         minlength: 9,
-        maxlength: 9
+        maxlength: 9,
       },
       inputValidator: (value) => {
         if (!value) {
@@ -139,29 +233,38 @@ $('#nuevaPlacaVisitante').click(function() {
         }else if(value.length <9){
           return 'Este campo está incompleto';
         }
-      }
+      },
+      footer: "El formato a seguir es: AAA-00-00"
     },
     {
-      input:'text',
+      input:'number',
       title: 'ID del Residente',
-      text: 'Escriba el ID del Residente',
+      inputAttributes: {
+        maxlength:10,
+        minlength:9999999999
+      },
+      text: 'Escriba el ID del residente al que se le asignará la placa',
       inputValidator: (value) => {
         if (!value) {
           return 'Este campo es requerido';
-        } else if (value.toString().length > 8 || value.toString().length <8){
-          return 'Este campo debe tener 8 digitos'
+        }else if(value.length <9){
+          return 'Este campo está incompleto';
+        }else if(value.length >10){
+          return 'Maximo dígitos superado'
         }
-      }
+      },
+      footer:'El ID residente debe ser el numero de telefono registrado',
     }
-  ]).then((result) => {
-    if(result.value) {
-      const placa = result.value[0]
-      const residentID = result.value[1]
+  ]).then(data => {
+    
+    if(data.value) {
+      const placa = data.value[0].toUpperCase()
+      const residentID = data.value[1]
       Swal.fire({
         icon:'info',
         title: 'Confirmación',
         html: `
-            ¿Desea dar de alta la placa ${placa} a nombre de ${residentID} en modo visitante?
+            ¿Desea dar de alta la placa <small>${placa}</small> con privilegios de visitante a nombre del residente con ID <small>${residentID}</small>?
         `,
         background:'#2c2c2c',
         confirmButtonColor:'#198754',
@@ -179,7 +282,10 @@ $('#nuevaPlacaVisitante').click(function() {
             }, 2000); // Simulación de una operación asincrónica
           });
         }
-
+      }).then((data) => {
+        if(data.isConfirmed){
+          setPlateVisit(placa,residentID)
+        }
       })
     }
   })
@@ -224,13 +330,17 @@ $('#nuevaExcepcion').click(function() {
           return 'Este campo es requerido';
         }
       }
-    },{
+    },
+    {
       input: 'text',
       title: 'Placa Excepción',
       text: 'Escriba los dígitos de la placa del vehiculo excepción:',
       inputAttributes:{
         minlength: 9,
         maxlength: 9
+      },
+      customClass : {
+        input: 'uppercase'
       },
       inputValidator: (value) => {
         if (!value) {
@@ -243,9 +353,9 @@ $('#nuevaExcepcion').click(function() {
   ]).then(data => {
     
     if(data.value) {
-      const tipo = data.value[0]
-      const descripcion = data.value[1]
-      const placa = data.value[2]
+      const tipo = data.value[0].toUpperCase()
+      const descripcion = data.value[1].toUpperCase()
+      const placa = data.value[2].toUpperCase()
       Swal.fire({
         icon:'info',
         title: 'Confirmación',
@@ -277,12 +387,11 @@ $('#nuevaExcepcion').click(function() {
   })
 });
 
-
 $('#abrirPuerta').click(function() {
   let timerInterval
   Swal.fire({
     title: 'PUERTA ABIERTA',
-    html: 'La puerta se cerrará en <b></b> <br>milisegundos',
+    html: 'La puerta se cerrará en <small></small> <br>milisegundos',
     timer: 10000, 
     background:'#2c2c2c',
     allowOutsideClick:false,
@@ -291,7 +400,7 @@ $('#abrirPuerta').click(function() {
     timerProgressBar: true,
     didOpen: () => {
       Swal.showLoading()
-      const b = Swal.getHtmlContainer().querySelector('b')
+      const b = Swal.getHtmlContainer().querySelector('small')
       timerInterval = setInterval(() => {
         b.textContent = Swal.getTimerLeft()
       }, 100)
